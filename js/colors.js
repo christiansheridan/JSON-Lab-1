3b3d400bc1d07ce6efbcc0ddf1546e3e08557dae
Christian Sheridan <christiansheridan@zipcoders-MacBook-Pro-13.local>
2018-11-28 17:05:03 -0500

    should be good

--- js/colors.js
+++ js/colors.js
@@ -4,7 +4,7 @@ var colorsArray : {
             "name":"green",
             "red":"64",
             "green":"175",
-            "blue":"63",
+            "blue":"63"
         },
         {
             "name":"yellow",
@@ -13,16 +13,16 @@ var colorsArray : {
             "blue":"62"
         },
         {
-            name:"red",
-            red:"192",
-            green:"46",
-            blue:"35"
-        }
+            "name":"red",
+            "red":"192",
+            "green":"46",
+            "blue":"35"
+        },
         {
             "name":"blue",
             "red":"19",
             "green":"71",
-            "blue":"144",
+            "blue":"144"
         },
         {
             "name"="orange",
